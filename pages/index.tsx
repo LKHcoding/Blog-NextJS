4a302c6e2162b7ea52e35673a732ad123da8404f
Kunhee Lim <tha920@naver.com>
2021-06-08 02:18:50 +0900

    index에서 markdown to html 제거
    md형식으로 작성된 텍스트를
    html형태로 꾸며서 보여줄 수 있는지
    확인했던 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,8 @@
-import MDEditor from '@uiw/react-md-editor';
-
 export default function Home() {
   return (
     <>
       index 페이지입니다.
       <div>{process.env.NEXT_PUBLIC_API_URL}</div>
-      <MDEditor.Markdown source={mkdStr} />
     </>
   );
 }
-
-const mkdStr = `![image](https://uicdn.toast.com/toastui/img/tui-editor-bi.png)
-
-# Awesome Editor!
-
-It has been _released as opensource in 2018_ and has ~~continually~~ evolved to **receive 10k GitHub ⭐️ Stars**.
-
-## Create Instance
-
-You can create an instance with the following code and use \`getHtml()\` and \`getMarkdown()\` of the [Editor](https://github.com/nhn/tui.editor).
-
-\`\`\`js
-const editor = new Editor(options);
-\`\`\`
-
-> See the table below for default options
-> > More API information can be found in the document
-
-| name | type | description |
-| --- | --- | --- |
-| el | \`HTMLElement\` | container element |
-
-## Features
-
-* CommonMark + GFM Specifications
-   * Live Preview
-   * Scroll Sync
-   * Auto Indent
-   * Syntax Highlight
-        1. Markdown
-        2. Preview
-
-## Support Wrappers
-
-> * Wrappers
->    1. [x] React
->    2. [x] Vue
->    3. [ ] Ember
-`;
