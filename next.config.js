aa786a7990e4796c64be090d36f6084cbc9817c5
Kunhee Lim <tha920@naver.com>
2021-07-06 02:18:44 +0900

    docs: Fix modules, settings
    uninstalled useless npm modules, next.config.js file,
    added settings in settings.json

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// next.config.js
-// const removeImports = require('next-remove-imports')();
-
-// module.exports = removeImports({});
-
-// const removeImports = require('next-remove-imports');
-
-// module.exports = removeImports({});
